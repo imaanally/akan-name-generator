0185ae5b3889a43c43c6f3b01933b62fc5f985d8
imaan <imaanally123@gmail.com>
2026-04-27 19:49:47 +0300

    Read form input values with JavaScript

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const result = document.querySelector("#result");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
+    const day = document.querySelector("#day").value;
+    const month = document.querySelector("#month").value;
+    const year = document.querySelector("#year").value;
+    const gender = document.querySelector("input[name='gender']:checked");
+
+    console.log(day);
+    console.log(month);
+    console.log(year);
+    console.log(gender);
 
     console.log("Form submitted");
 });
